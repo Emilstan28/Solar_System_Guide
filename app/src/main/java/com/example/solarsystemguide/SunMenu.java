f070b9344050e37d4fc8bf14bff4a85265bf7abd
Emil <47715111+Emilstan28@users.noreply.github.com>
2021-01-12 17:18:24 +0100

    Completed the sun menu, added the images to gallery for the sun, will add details of the body and missions that went after it next time

--- app/src/main/java/com/example/solarsystemguide/SunMenu.java
+++ app/src/main/java/com/example/solarsystemguide/SunMenu.java
@@ -2,7 +2,10 @@ package com.example.solarsystemguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SunMenu extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class SunMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sun_menu);
+
+        Button learnMoreSunInfo = findViewById(R.id.buttonLearnMoreSunInformation);
+
+        learnMoreSunInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toSunDetails();
+            }
+        });
     }
+
+
+    public void toSunDetails(){
+        Intent intent = new Intent(this, SunDetails.class);
+        startActivity(intent);
+    }
+
 }
