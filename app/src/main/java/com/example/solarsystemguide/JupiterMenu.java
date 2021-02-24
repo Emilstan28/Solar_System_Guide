fd10aa9d7931d20b7005788e1e0080b1d0871439
Emil <47715111+Emilstan28@users.noreply.github.com>
2021-02-24 16:12:21 +0100

    Started describing past Jupiter missions
    Added the fragment for them
    
    Implemented the Jupiter mission menu

--- app/src/main/java/com/example/solarsystemguide/JupiterMenu.java
+++ app/src/main/java/com/example/solarsystemguide/JupiterMenu.java
@@ -2,13 +2,48 @@ package com.example.solarsystemguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class JupiterMenu extends AppCompatActivity {
+
+    Button learnMoreMissions, learnMoreJupiterInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jupiter_menu);
+
+
+        learnMoreJupiterInfo = findViewById(R.id.buttonLearnMoreJupiterInformation);
+        learnMoreMissions = findViewById(R.id.buttonLearnMoreJovianMissions);
+
+        learnMoreJupiterInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toJupiterDetails();
+            }
+        });
+
+        learnMoreMissions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toJupiterMissions();
+            }
+        });
+    }
+
+
+
+    public void toJupiterDetails(){
+        Intent intent = new Intent(this, JupiterDetails.class);
+        startActivity(intent);
+    }
+
+    public void toJupiterMissions(){
+        Intent intent = new Intent(this, JupiterResearchMissions.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/solarsystemguide/JupiterResearchMissions.java
@@ -0,0 +1,56 @@
+package com.example.solarsystemguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class JupiterResearchMissions extends AppCompatActivity {
+
+    Button pastMissionsButton, ongoingMissionsButton;
+    Fragment pastJupiterMissions, ongoingJupiterMissions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jupiter_research_missions);
+
+        pastMissionsButton = findViewById(R.id.endedMissionsJupiterButton);
+        pastMissionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onEndedMissionsClick();
+            }
+        });
+
+        ongoingMissionsButton = findViewById(R.id.ongoingMissionsJupiterButton);
+        ongoingMissionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onOngoingMissionsClick();
+            }
+        });
+
+
+
+
+        pastJupiterMissions = new Fragment(R.layout.fragment_ended_research_jupiter_missions);
+        //ongoingJupiterMissions = new Fragment(R.layout.fragment_ongoing_research_jupiter_missions);
+    }
+
+
+    public void onEndedMissionsClick(){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameForFragmentsJupiterMissions, pastJupiterMissions);
+        transaction.commit();
+    }
+
+    public void onOngoingMissionsClick(){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameForFragmentsJupiterMissions, ongoingJupiterMissions);
+        transaction.commit();
+    }
+}
